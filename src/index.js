f7f5ca3865cdeb26e052b18fa4041a82dfe0898c
Dmitriy Glebov <120340349+restgoss@users.noreply.github.com>
2023-09-17 17:34:29 +0300

    add blank page top scroll

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import ScrollToTop from './utils/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
+    <ScrollToTop />
     <App />
     </HashRouter>
   </React.StrictMode>
